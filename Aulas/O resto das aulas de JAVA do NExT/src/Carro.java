43ad324ad9e53f495a7b3ae8b6b928dd778148e3
DaniloRudrigues <danilo.rudrigues@gmail.com>
2022-05-10 21:59:58 -0300

    aula 6

---
+++ Aulas/O resto das aulas de JAVA do NExT/src/Carro.java
@@ -0,0 +1,12 @@
+public class Carro {
+    private String placa;
+    private int numChassi;
+
+    public void acelerar(){
+        
+    }
+    
+    public boolean frear(){
+        return true;
+    }
+}
